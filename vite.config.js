6828953280c949b68d160bd97a67212db7671408
makia98 <makia98@foxmail.com>
2026-02-17 00:23:54 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   root: 'src_html',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src_html/index.html'),
+        human_vs_ai: resolve(__dirname, 'src_html/human_vs_ai.html'),
+        ai_vs_ai: resolve(__dirname, 'src_html/ai_vs_ai.html'),
+        rules: resolve(__dirname, 'src_html/rules.html'),
+      },
+    },
   },
   preview: {
     port: 4173
